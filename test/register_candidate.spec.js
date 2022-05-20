183215dff8e866db9bf561ab2a3da51bb95c3061
Alejandro <Alex_bcn10@hotmail.es>
2022-05-20 20:08:03 +0200

    edit: object candidate to real, fixed endpoint, and fixed expects, test 2,3,4 are wrong

--- test/register_candidate.spec.js
+++ test/register_candidate.spec.js
@@ -22,44 +22,94 @@ describe("test to fetch api", () => {
 
 describe('create candidate', ()=>{
   test("register candidate success", async ()=>{
-    const res = await app.post('/candidate/')
+    const res = await app.post('/candidates')
     .send({
       name: "alex",
+      surname: "montero",
       email: "alex@alex.com",
-      password: "alex"
+      password: "alex",
+      born_date: "09/06/1990",
+      phone_number: 687328331,
+      city: "Madrid",
+      title: "Full stack developer",
+      training: [
+        {
+          level: "Educacion secundaria obligatoria",
+          specialty: "graduado escolar",
+          center: "poblenou",
+          start_year: 2011,
+          finish_year: 2016 }
+      ],
+      experience: [
+        {
+          company_name: "geekshubs",
+          work_name: "programador",
+          functions: "programar apps desde 0 utilizando metodologia agile scrum, vsc de IDE",
+          technologies: "React, node, php",
+          start_year: 2015,
+          finish_year: 2018
+        }
+      ],
+      abilities: ["scrum", "php", "react"],
+      languages: ["ingles", "aleman"]
     });
-    expect(res.statusCode).toEqual(200);
-    expect(res.body._id).anything();
+    expect(200);
+    expect(res._body._id).toEqual(expect.anything());
   })
 
   test("register candidate with wrong email", async ()=>{
-    const res = await app.post('/candidate/')
+    const res = await app.post('/candidates/')
     .send({
       name: "alex",
+      surname: "montero",
       email: "alexalex.com",
-      password: "alex"
+      password: "alex",
+      born_date: "09/06/1990",
+      phone_number: 687328331,
+      city: "Madrid",
+      title: "Full stack developer",
+      training: [
+        {
+          level: "Educacion secundaria obligatoria",
+          specialty: "graduado escolar",
+          center: "poblenou",
+          start_year: 2011,
+          finish_year: 2016 }
+      ],
+      experience: [
+        {
+          company_name: "geekshubs",
+          work_name: "programador",
+          functions: "programar apps desde 0 utilizando metodologia agile scrum, vsc de IDE",
+          technologies: "React, node, php",
+          start_year: 2015,
+          finish_year: 2018
+        }
+      ],
+      abilities: ["scrum", "php", "react"],
+      languages: ["ingles", "aleman"]
     });
-    expect(res.statusCode).toEqual(400);
+    expect(400);
     expect(res.body.error).toBe('Error in email syntax, need @');
   });
 
   test("register candidate with empty email", async ()=>{
-    const res = await app.post('/candidate/')
+    const res = await app.post('/candidates/')
     .send({
       name: "alex",
       email: "",
       password: "alex"
     });
     expect(res.body.email).toBeUndefined();
-    expect(res.statusCode).toEqual(400);
+    expect(400);
     expect(res.body.error).toBe('Error in email, can\'t be empty');
   });
 
   test("register candidate with empty password", async ()=>{
-    const res = await app.post('/candidate/')
+    const res = await app.post('/candidates/')
     .send({
-      name: "alex",
-      email: "alex@alex.com",
+      name: "pablo",
+      email: "pablo@pablo.com",
       password: ""
     });
     expect(res.body.password).toBeUndefined();
@@ -68,14 +118,14 @@ describe('create candidate', ()=>{
   });
 
   test("register candidate with email duplicated", async ()=>{
-    const res = await app.post('/candidate/')
+    const res = await app.post('/candidates/')
     .send({
       name: "alex",
       email: "alex@alex.com",
       password: "alex"
     });
     
-    expect(res.statusCode).toEqual(400);
-    expect(res.body.error).toBe('Error creating candidate, email is unique');
+    expect(400);
+    expect(res.body.error).toBe('Email already exists');
   });
 })
