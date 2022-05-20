0da2d2ab8c9482fabc7673b7213cea14bab0cea3
Alejandro <Alex_bcn10@hotmail.es>
2022-05-20 14:41:13 +0200

    edit: imports to requires to fix an jest error with modules

--- DB/connection.js
+++ DB/connection.js
@@ -1,11 +1,9 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
-const connection = async () =>{
+module.exports.connection = async () =>{
     try{
         await mongoose.connect('mongodb://localhost:27017/mytests')
     }catch(error){
         console.log(error)
     }
 }
-
-export default connection;
