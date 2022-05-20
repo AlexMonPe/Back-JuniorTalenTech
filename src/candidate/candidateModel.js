f5d38f147535fc1ccd0c4bf9558032cd0776c1a6
Alejandro <Alex_bcn10@hotmail.es>
2022-05-20 16:56:55 +0200

    create: candidate model

---
+++ src/candidate/candidateModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+// Schema: Candidate
+const Schema = new mongoose.Schema({
+  name: {type: String, required: true},
+  surname: {type: String, required: true},
+  born_date: {type: String, required: true},
+  phone_number: {type: Number, required: true, unique: true},
+  city: {type: String, required: true},
+  title: String,
+  training: [
+    {
+      level: {type: String, required: true},
+      specialty: {type: String, required: true},
+      center: {type: String, required: true},
+      start_year: {type: Number, required: true},
+      finish_year: {type: Number, required: true},
+    },
+  ],
+  experience: [{
+      company_name: {type: String, required: true},
+      work_name: {type: String, required: true},
+      functions: {type: String, text: true, required: true},
+      tecnologies: String,
+      start_year: {type: Number, required: true},
+      finish_year: {type: Number, required: true},
+  }],
+  abilities: [{type: String, required: true}],
+  languages: [String]
+});
+module.exports.Candidate = mongoose.model("Candidate", Schema);
