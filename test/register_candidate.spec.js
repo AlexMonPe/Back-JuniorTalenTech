2ffaf1b4402e240f04a8b455b5457cec78a79d6a
Alejandro <Alex_bcn10@hotmail.es>
2022-05-20 14:41:53 +0200

    add: tests of empty pwd and email, and duplicated email

--- test/register_candidate.spec.js
+++ test/register_candidate.spec.js
@@ -1,6 +1,6 @@
 const supertest = require("supertest");
 const { server } = require("../index.js");
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const app = supertest(server);
 
@@ -31,6 +31,7 @@ describe('create candidate', ()=>{
     expect(res.statusCode).toEqual(200);
     expect(res.body._id).anything();
   })
+
   test("register candidate with wrong email", async ()=>{
     const res = await app.post('/candidate/')
     .send({
@@ -40,5 +41,46 @@ describe('create candidate', ()=>{
     });
     expect(res.statusCode).toEqual(400);
     expect(res.body.error).toBe('Error in email syntax, need @');
+  });
+
+  test("register candidate with empty email", async ()=>{
+    const res = await app.post('/candidate/')
+    .send({
+      name: "alex",
+      email: "",
+      password: "alex"
+    });
+    expect(res.body.email).toBeUndefined();
+    expect(res.statusCode).toEqual(400);
+    expect(res.body.error).toBe('Error in email, can\'t be empty');
+  });
+
+  test("register candidate with empty password", async ()=>{
+    const res = await app.post('/candidate/')
+    .send({
+      name: "alex",
+      email: "alex@alex.com",
+      password: ""
+    });
+    expect(res.body.password).toBeUndefined();
+    expect(res.statusCode).toEqual(400);
+    expect(res.body.error).toBe('Error in password, can\'t be empty');
+  });
+
+  test("register candidate with email duplicated", async ()=>{
+    const res = await app.post('/candidate/')
+    .send({
+      name: "alex",
+      email: "alex@alex.com",
+      password: "alex"
+    },
+    {
+      name: "pablo",
+      email: "alex@alex.com",
+      password: "pablo"
+    });
+    
+    expect(res.statusCode).toEqual(400);
+    expect(res.body.error).toBe('Error creating candidate, email is unique');
   });
 })
