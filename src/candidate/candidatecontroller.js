5ddd2674b347d168b0466cee71315ae7b1dd249b
Alejandro <Alex_bcn10@hotmail.es>
2022-05-20 18:19:05 +0200

    edit: added user create inside create candidate and check if email exists

--- src/candidate/candidatecontroller.js
+++ src/candidate/candidatecontroller.js
@@ -1,11 +1,28 @@
+const Users = require("../users/userModel.js");
 const Candidates = require("./candidateModel.js");
 
-const createCandidate = async (req,res) => {
+const createCandidate = async (req, res) => {
   try {
-    const candidateCreated = await Candidates.create(req.body);
-    res.status(200).json(candidateCreated);
+    const userToCreate = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      role: "candidate",
+    };
+
+    const isEmailDuplicated = await Users.findOne({ email: req.body.email });
+
+    if (!isEmailDuplicated) {
+      await Users.create(userToCreate);
+
+      const candidateCreated = await Candidates.create(req.body);
+
+      res.status(200).json(candidateCreated);
+    } else {
+      res.status(400).json("Email already exists");
+    }
   } catch (error) {
-    res.json(error);
+    res.status(400).json(error, "Error creating candidate or user");
   }
 };
 
