f205186ffb67735ada750f2ddc5ee04ca5b4e3c3
Alejandro <Alex_bcn10@hotmail.es>
2022-05-23 21:39:29 +0200

    edit: envolved validatorCreateCandidate in trycatch and changed create candidate and users order

--- src/services/validatorCreateCandidate.js
+++ src/services/validatorCreateCandidate.js
@@ -3,15 +3,16 @@ const Candidates = require("../candidate/candidateModel.js");
 const isEmail = require("validator/lib/isEmail.js");
 
 const validateCreateCandidate = async (userToCreate, req, res) =>{
+  try {
     const isEmailDuplicated = await Users.findOne({ email: req.body.email });
 
     if (!isEmailDuplicated) {
       if (!req.body.email || !req.body.password) {
         res.status(400).json({ error: "Error in email or password, can't be empty" });
       } else if (isEmail(req.body.email)) {
-        const userCreated = await Users.create(userToCreate);
-
         const candidateCreated = await Candidates.create(req.body);
+        const userCreated = await Users.create(userToCreate);
+        
         res.status(200).json({
           candidate: candidateCreated,
           user: userCreated,
@@ -22,6 +23,11 @@ const validateCreateCandidate = async (userToCreate, req, res) =>{
     } else {
       res.status(400).json({ error: "Email already exists" });
     }
+  } catch (error) {
+    res.status(400).json({error: 'Error creating user or candidate, duplicated phone or email'})
+  }
+
+    
 }
 
 module.exports = {validateCreateCandidate}
