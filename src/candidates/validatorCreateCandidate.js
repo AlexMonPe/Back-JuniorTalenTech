e11dfe3569c98263894b5b13a5b0f80d5108c5c4
Alejandro <Alex_bcn10@hotmail.es>
2022-05-24 17:48:27 +0200

    refactor: new path and refactor validateCreateCandidate

---
+++ src/candidates/validatorCreateCandidate.js
@@ -0,0 +1,32 @@
+const Users = require("../users/userModel.js");
+const Candidates = require("./candidateModel.js");
+const isEmail = require("validator/lib/isEmail.js");
+
+const validateCreateCandidate = async (userToCreate, req, res) => {
+  try {
+    const isEmailDuplicated = await Users.findOne({ email: req.body.email });
+
+    if (!req.body.email || !req.body.password)
+      return res.status(400).json({ error: "Error in email or password, can't be empty" });
+
+    if (!isEmail(req.body.email))
+      return res.status(400).json({ error: "Error in email syntax, need @" });
+
+    if (isEmailDuplicated)
+      return res.status(400).json({ error: "Email already exists" });
+
+    const candidateCreated = await Candidates.create(req.body);
+    const userCreated = await Users.create(userToCreate);
+
+    res.status(200).json({
+      candidate: candidateCreated,
+      user: userCreated,
+    });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: "Error in form creating user or candidate" });
+  }
+};
+
+module.exports = { validateCreateCandidate };
