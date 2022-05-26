045d0f4340543549299990c7c925ab2e19819f71
Alejandro <Alex_bcn10@hotmail.es>
2022-05-27 00:23:51 +0200

    edit: req.body to .form, changed in model

--- src/candidates/candidatecontroller.js
+++ src/candidates/candidatecontroller.js
@@ -1,17 +1,16 @@
-const { validateCreateCandidate } = require("./validatorCreateCandidate.js")
-const {hasher} = require("../services/bcrypt.js")
+const { validateCreateCandidate } = require("./validatorCreateCandidate.js");
+const { hasher } = require("../services/bcrypt.js");
 
 const createCandidate = async (req, res) => {
   try {
     const userToCreate = {
-      name: req.body.name,
-      email: req.body.email,
-      password: await hasher(req.body.password),
+      name: req.body.form.name,
+      email: req.body.form.email,
+      password: await hasher(req.body.form.password),
       role: "candidate",
     };
 
     validateCreateCandidate(userToCreate, req, res);
-    
   } catch (error) {
     res.status(400).json(error, "Error creating candidate or user");
   }
