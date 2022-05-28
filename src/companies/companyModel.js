2e3291c97142aca4f8e8a91769dce585204b9585
Alejandro <Alex_bcn10@hotmail.es>
2022-05-28 13:33:32 +0200

    add: model of company

---
+++ src/companies/companyModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Users = require("../users/userModel.js");
+
+// Schema: Company
+const Schema = new mongoose.Schema({
+  // idUser: { type: mongoose.Schema.Types.ObjectId, ref: "Users", required: true },
+  name: { type: String, required: true },
+  title: String,
+  description: String,
+  ubication: { type: String, required: true },
+  website: String,
+  cif: String,
+  phone_number: { type: Number, required: true },
+  employees: String,
+});
+
+module.exports.Companies = mongoose.model("Companies", Schema);
