d9a516891e499fd6fe856d177c5239f5e0440cee
Alejandro <Alex_bcn10@hotmail.es>
2022-05-28 16:31:53 +0200

    edit: deleted validator file and all logic into candidate controller, bc controller not return user created

--- src/candidates/candidatecontroller.js
+++ src/candidates/candidatecontroller.js
@@ -1,5 +1,7 @@
-const { validateCreateCandidate } = require("./validatorCreateCandidate.js");
 const { hasher } = require("../services/bcrypt.js");
+const { Users } = require("../users/userModel.js");
+const { Candidates } = require("./candidateModel.js");
+const isEmail = require("validator/lib/isEmail.js");
 
 const createCandidate = async (req, res) => {
   try {
@@ -10,7 +12,32 @@ const createCandidate = async (req, res) => {
       role: "candidate",
     };
 
-    validateCreateCandidate(userToCreate, req, res);
+    const isEmailDuplicated = await Users.findOne({
+      email: req.body.form.email,
+    });
+
+    if (!req.body.form.email || !req.body.form.password)
+      return res
+        .status(400)
+        .json({ error: "Error in email or password, can't be empty" });
+
+    if (!isEmail(req.body.form.email))
+      return res.status(400).json({ error: "Error in email syntax, need @" });
+
+    if (isEmailDuplicated)
+      return res.status(400).json({ error: "Email already exists" });
+
+    const userCreated = await Users.create(userToCreate);
+
+    if (!userCreated)
+      return res.status(400).json({ error: "Error creating user" });
+
+    const candidateCreated = await Candidates.create(req.body);
+
+    res.status(200).json({
+      candidate: candidateCreated,
+      user: userCreated,
+    });
   } catch (error) {
     res.status(400).json(error, "Error creating candidate or user");
   }
