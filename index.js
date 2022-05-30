219427a6abfa9c62ac489b2a607cd8172543dfb6
Alejandro <Alex_bcn10@hotmail.es>
2022-05-30 15:15:00 +0200

    edit. indented index.js

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const express = require('express');
-const connection = require('./DB/connection.js')
-const routerCandidates = require('./src/candidates/candidateRoute.js')
-const routerUsers = require('./src/users/userRoute.js')
-const routerCompanies = require('./src/companies/companyRoute.js')
-const cors = require('cors')
+const express = require("express");
+const { connection } = require("./DB/connection.js");
+const routerCandidates = require("./src/candidates/candidateRoute.js");
+const routerUsers = require("./src/users/userRoute.js");
+const routerCompanies = require("./src/companies/companyRoute.js");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-connection().then(()=> console.log('Database is up')).catch('Error connecting DB');
+connection()
+  .then(() => console.log("Database is up"))
+  .catch("Error connecting DB");
 
-app.get('/api/echo', (req,res) => res.json({echo: 'test get works'}));
+app.get("/api/echo", (req, res) => res.json({ echo: "test get works" }));
 
-app.use('/candidates', routerCandidates);
-app.use('/users', routerUsers);
-app.use('/companies', routerCompanies);
+app.use("/candidates", routerCandidates);
+app.use("/users", routerUsers);
+app.use("/companies", routerCompanies);
 
+app.set("port", process.env.PORT || 1515);
 
-app.set("port", process.env.PORT || 1515)
-
-module.exports.server = app.listen(app.get("port"), () => { 
-    console.log("Server up at " + process.env.SERVER_PORT)
-})
+module.exports.server = app.listen(app.get("port"), () => {
+  console.log("Server up at " + process.env.SERVER_PORT);
+});
