76d965110dc9c2f0fefbc7dea4cd23ca724a0e2b
Alejandro <Alex_bcn10@hotmail.es>
2022-05-30 18:35:20 +0200

    add: get candidate by userid route

--- src/candidates/candidateRoute.js
+++ src/candidates/candidateRoute.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { createCandidate, updateCandidate } = require("./candidateController.js");
+const { createCandidate, updateCandidate, getCandidateByUserId } = require("./candidateController.js");
 
 
 //Create candidate.
 router.post('/', createCandidate);
 router.patch('/:id', updateCandidate)
+router.get('/:id', getCandidateByUserId)
+
 
 module.exports = router;
