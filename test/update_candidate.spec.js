730b840ad8ba3d3e1614fd3c223119f00a93c81d
Alejandro <Alex_bcn10@hotmail.es>
2022-05-30 15:11:42 +0200

    edit. tests responses from controller, and ids

--- test/update_candidate.spec.js
+++ test/update_candidate.spec.js
@@ -9,12 +9,10 @@ afterAll(() => mongoose.disconnect());
 
 describe("update candidate", () => {
   test("update candidate success", async () => {
-    const res = await app.patch("/candidates/1").send({
+    const res = await app.patch("/candidates/6294baded847c1dcf6360361").send({
       form: {
         name: "alex",
         surname: "montero",
-        email: "alex@geekshubs.com",
-        password: "123456",
         born_date: "09/06/1990",
         phone_number: 687328331,
         city: "Madrid",
@@ -48,82 +46,49 @@ describe("update candidate", () => {
       ],
     });
     expect(200);
-    expect(res.body.user.email).toBe("alex@geekshubs.com");
-    expect(res.body.candidate.language_name).toBe("aleman");
-    expect(res.body.candidate.work_name).toBe("developer");
+    expect(res.body).toBe("Se han guardado los cambios");
+  });
+
+  test("update user success", async () => {
+    const res = await app.patch("/users/6294baded847c1dcf636035f").send({
+      name: "alex",
+      email: "alex@geekshubs.com",
+      password: "123456",
+    });
+    expect(200);
+    expect(res.body).toBe("Se han guardado los cambios");
   });
 
   test("update candidate with wrong id", async () => {
     const res = await app.patch("/candidates/99").send({
-      form: { name: "alex", email: "alex@alex.com", password: "alex" },
+      name: "alex",
+      surname: "perez",
+      born_date: "09/06/1990",
+      phone_number: 687328331,
+      city: "Madrid",
+      title: "Full stack developer",
     });
     expect(400);
-    expect(res.body.error).toBe("User not found");
+    expect(res.body.error).toBe("Usuario no encontrado");
   });
 
-  test("update candidate with wrong email", async () => {
-    const res = await app.patch("/candidates/1").send({
-      form: {
-        name: "alex",
-        surname: "montero",
-        email: "alexalex.com",
-        password: "alex",
-        born_date: "09/06/1990",
-        phone_number: 687328331,
-        city: "Madrid",
-        title: "Full stack developer",
-      },
-      training: [
-        {
-          level: "Educacion secundaria obligatoria",
-          specialty: "graduado escolar",
-          center: "poblenou",
-          start_year: 2011,
-          finish_year: 2016,
-        },
-      ],
-      experience: [
-        {
-          company_name: "geekshubs",
-          work_name: "programador",
-          functions:
-            "programar apps desde 0 utilizando metodologia agile scrum, vsc de IDE",
-          start_year: 2015,
-          finish_year: 2018,
-        },
-      ],
-      abilities: ["scrum", "php", "react"],
-      languages: ["ingles", "aleman"],
+  test("update user with wrong id", async () => {
+    const res = await app.patch("/candidates/99").send({
+      name: "alex",
+      email: "alex@alex.com",
+      password: "1234567",
     });
     expect(400);
-    expect(res.body.error).toBe("Error in email syntax, need @");
+    expect(res.body.error).toBe("Usuario no encontrado");
   });
 
-  test("update candidate with empty email", async () => {
-    const res = await app.patch("/candidates/1").send({
-      form: { name: "alex", email: "", password: "alex" },
+  test("update user with wrong email", async () => {
+    const res = await app.patch("/users/6294baded847c1dcf636035f").send({
+      name: "alex",
+      email: "alexgeekshubs.com",
+      password: "123456",
     });
-    expect(res.body.email).toBeUndefined();
     expect(400);
-    expect(res.body.error).toBe("Error in email or password, can't be empty");
+    expect(res.body.error).toBe("Email no valido");
   });
-
-  test("update candidate with empty password", async () => {
-    const res = await app.patch("/candidates/1").send({
-      form: { name: "pablo", email: "pablo@pablo.com", password: "" },
-    });
-    expect(res.body.password).toBeUndefined();
-    expect(res.statusCode).toEqual(400);
-    expect(res.body.error).toBe("Error in email or password, can't be empty");
-  });
-
-  test("update candidate with email duplicated", async () => {
-    const res = await app.patch("/candidates/2").send({
-      form: { name: "alex", email: "alex@alex.com", password: "alex" },
-    });
-
-    expect(400);
-    expect(res.body.error).toBe("Email is already in use");
-  });
-
 });
