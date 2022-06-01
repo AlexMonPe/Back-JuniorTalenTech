c60c377c3067fb65d71ffa651a821d785c545112
Alejandro <Alex_bcn10@hotmail.es>
2022-06-01 21:32:18 +0200

    add: route of update company

--- src/companies/companyRoute.js
+++ src/companies/companyRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { createCompany, getCompanyByUserId } = require("./companyController.js");
+const { createCompany, getCompanyByUserId, updateCompany } = require("./companyController.js");
 
 //Create company.
 router.post("/", createCompany);
-router.get('/:id', getCompanyByUserId)
+router.get('/:id', getCompanyByUserId);
+router.patch('/:id', updateCompany);
+
 
 module.exports = router;
