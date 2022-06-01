0374e696e2a4140f8e4101369daed308132c9b69
Alejandro <Alex_bcn10@hotmail.es>
2022-06-01 21:14:26 +0200

    add: route of get company by user

--- src/companies/companyRoute.js
+++ src/companies/companyRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { createCompany } = require("./companyController.js");
+const { createCompany, getCompanyByUserId } = require("./companyController.js");
 
 //Create company.
 router.post("/", createCompany);
+router.get('/:id', getCompanyByUserId)
 
 module.exports = router;
