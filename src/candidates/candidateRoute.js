5c703b54d0a3fa9de40d619ee7afed41154e5ff7
Alejandro <Alex_bcn10@hotmail.es>
2022-06-03 16:24:12 +0200

    add: getcandidates route

--- src/candidates/candidateRoute.js
+++ src/candidates/candidateRoute.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { createCandidate, updateCandidate, getCandidateByUserId } = require("./candidateController.js");
+const { createCandidate, updateCandidate, getCandidateByUserId, getAllCandidates } = require("./candidateController.js");
 
 
 //Create candidate.
 router.post('/', createCandidate);
 router.patch('/:id', updateCandidate)
 router.get('/:id', getCandidateByUserId)
+router.get('/', getAllCandidates)
+
 
 
 module.exports = router;
