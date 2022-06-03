eadaf69b3d1e61799b633c686e76cfeb1101682d
Alejandro <Alex_bcn10@hotmail.es>
2022-06-03 20:52:19 +0200

    add: getCandidatebyId route and changed endpoint of getCandidateByIserId

--- src/candidates/candidateRoute.js
+++ src/candidates/candidateRoute.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { createCandidate, updateCandidate, getCandidateByUserId, getAllCandidates } = require("./candidateController.js");
+const { createCandidate, updateCandidate, getCandidateByUserId, getAllCandidates, getCandidateById } = require("./candidateController.js");
 
 
 //Create candidate.
 router.post('/', createCandidate);
 router.patch('/:id', updateCandidate)
-router.get('/:id', getCandidateByUserId)
+router.get('/user/:id', getCandidateByUserId)
 router.get('/', getAllCandidates)
+router.get('/:id', getCandidateById)
+
 
 
 
